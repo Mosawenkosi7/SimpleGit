88d3b7ab1920dcec18732bb1f8bb8de3a0187c62
Mosa Msiza <Mosamsiza.29@gmail.com>
2024-10-31 12:40:08 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("I started the wrong projec, but i think this can still work");
+Console.WriteLine("Lesson 1: How to loop over an array");
+
+string[] provinces = { "Gauteng", "Limpopo", "Kwa-zulu natal", "Mpumalanga"};
+foreach(string province in provinces)
+{
+    Console.WriteLine(province);
+}
